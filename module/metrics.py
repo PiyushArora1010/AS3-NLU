89538580f5d9dcfe08fcf776e8f6ce11351544ae
Piyush Arora <arora.8@iitj.ac.in>
2024-04-18 23:47:50 +0530

    changes

--- module/metrics.py
+++ module/metrics.py
@@ -1,5 +1,9 @@
 import evaluate
+import nltk
+
+nltk.download("punkt", quiet=True)
 
 metricDic = {
-    "bleu": evaluate.load('bleu')
+    "bleu": evaluate.load('bleu'),
+    "rouge": evaluate.load('rouge'),
 }
