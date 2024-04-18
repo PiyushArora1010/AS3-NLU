fd7bf4d9d3d02f3989eea9677144f905b7fc6859
Piyush Arora <arora.8@iitj.ac.in>
2024-04-18 14:09:42 +0530

    changes

--- module/metrics.py
+++ module/metrics.py
@@ -1,7 +1,6 @@
 import evaluate
 
 metricDic = {
-    'rouge': evaluate.load('rouge'),
     'bleu': evaluate.load('bleu'),
     'meteor': evaluate.load('meteor'),
     'cider': evaluate.load('cider')
